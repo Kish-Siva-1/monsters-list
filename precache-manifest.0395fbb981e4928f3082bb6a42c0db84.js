1e1726058cd85879aff1169cb1383e4f43370605
Kish-Siva-1 <rodes88@msn.com>
2020-04-19 13:06:05 +0200

    Updates

---
+++ precache-manifest.0395fbb981e4928f3082bb6a42c0db84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4710b9a24df1b4e687c91b4c1d7f3f",
+    "url": "/monsters-list/index.html"
+  },
+  {
+    "revision": "1a99634623dac2e97682",
+    "url": "/monsters-list/static/css/main.70353b11.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-list/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-list/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a99634623dac2e97682",
+    "url": "/monsters-list/static/js/main.7cb09657.chunk.js"
+  },
+  {
+    "revision": "3d90ffa706dd39a50d4b",
+    "url": "/monsters-list/static/js/runtime-main.c9cd4042.js"
+  }
+]);
